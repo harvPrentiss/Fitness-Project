a810556e859b6c732be1f6a69627dc4c38a63194
Harold Prentiss <harv.prentiss@gmail.com>
2014-01-22 21:29:42 -0500

    Changes to layout

--- application.js
+++ application.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-	$('#leftBox').on('click', '.boxHeader' function(event){
+	$('#soundtrackHeader').on('click', function(event){
 		event.preventDefault();
-		$(this).closest('div');
+		$(this).closest('div').find('.soundtrack').fadeToggle('fast');
+	});
+
+	$('#excerciseHeader').on('click', function(event){
+	event.preventDefault();
+	$(this).closest('div').find('ul').fadeToggle('fast');
 	});
 });
