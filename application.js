c3ce854a35896e190b6255888a5f515b6c79f703
Harold Prentiss <harv.prentiss@gmail.com>
2014-01-24 23:20:30 -0500

    More cosmetic and design changes.

--- application.js
+++ application.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 	$('#soundtrackHeader').on('click', function(event){
 		event.preventDefault();
-		$(this).closest('div').find('.soundtrack').fadeToggle('fast');
+		$(this).closest('div').find('.soundtrack').fadeToggle('fast', 'swing');
 	});
 
 	$('#excerciseHeader').on('click', function(event){
 	event.preventDefault();
-	$(this).closest('div').find('ul').fadeToggle('fast');
+	$(this).closest('div').find('ul').fadeToggle('fast', 'swing');
 	});
 });
