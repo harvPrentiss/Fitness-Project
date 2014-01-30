aeb2b64d44ef1c42facb83880bccf553fce1949a
Harold Prentiss <harv.prentiss@gmail.com>
2014-01-30 18:16:15 -0500

    Cosmetic changes again
    Added the profile card as the left content

--- application.js
+++ application.js
@@ -1,11 +1,3 @@
 $(document).ready(function(){
-	$('#soundtrackHeader').on('click', function(event){
-		event.preventDefault();
-		$(this).closest('div').find('.soundtrack').fadeToggle('fast', 'swing');
-	});
-
-	$('#excerciseHeader').on('click', function(event){
-	event.preventDefault();
-	$(this).closest('div').find('ul').fadeToggle('fast', 'swing');
-	});
+	
 });
