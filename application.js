0b129781054c06ba9b023e157744df968e298c2e
Harold Prentiss <harv.prentiss@gmail.com>
2014-01-30 19:03:20 -0500

    Did nothing really
    Need to make the card flip happen on click of a button or link.

--- application.js
+++ application.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-	
+	$('.flipperButtonFront').on('mouseover', function(){
+		$('.front').addClass('frontFlipped');
+		console.log('front flipped');
+		$('.back').addClass('backFlipped');
+		console.log('back flipped');
+	})
 });
